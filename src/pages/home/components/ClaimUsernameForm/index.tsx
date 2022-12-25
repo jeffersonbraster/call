fdc60f423a7ffb684accb99a24283c08d94f8588
Jefferson <jeferson.oliveira@g.globo>
2022-12-25 17:10:58 -0300

    config react-hook-form and zod

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,14 +1,29 @@
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 import { Button, TextInput } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
 import { Form } from './styles'
 
+const claimUsernameFormSchema = z.object({
+  username: z.string().min(3).max(10),
+})
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
+
 export function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm<ClaimUsernameFormData>()
+
+  async function handleClaimUsername({ username }: ClaimUsernameFormData) {
+    console.log(username)
+  }
+
   return (
-    <Form as="form">
+    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
       <TextInput
         size="sm"
         prefix="call.jeffersonbr../"
         placeholder="seu-usuário"
+        {...register('username')}
       />
       <Button size="sm" type="submit">
         Reservar
