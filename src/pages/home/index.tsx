00b1256c4b7b15e9ceb8bf24442776179032f15e
Jefferson <jeferson.oliveira@g.globo>
2022-12-25 17:05:21 -0300

    create component ClaimUsernameForm

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,6 +2,7 @@ import { Heading, Text } from '@ignite-ui/react'
 import Image from 'next/image'
 
 import previewImg from '../../assets/calendarImg.png'
+import { ClaimUsernameForm } from './components/ClaimUsernameForm'
 import { Container, Hero, Preview } from './styles'
 
 export default function Home() {
@@ -16,6 +17,8 @@ export default function Home() {
           Conecte seu calendário e permita que as pessoas marquem agendamentos
           no seu tempo livre.
         </Text>
+
+        <ClaimUsernameForm />
       </Hero>
 
       <Preview>
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -15,13 +15,13 @@ export const Hero = styled('div', {
   maxWidth: 480,
   padding: '0 $10',
 
-  [`${Heading}`]: {
+  [`> ${Heading}`]: {
     '@media (max-width: 600px)': {
       fontSize: '$6xl',
     },
   },
 
-  [`${Text}`]: {
+  [`> ${Text}`]: {
     marginTop: '$2',
     color: '$gray200',
     fontSize: '$xl',
