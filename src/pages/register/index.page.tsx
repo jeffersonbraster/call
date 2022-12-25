13c21a1f87d240cb9168935750617273c658f605
Jefferson <jeferson.oliveira@g.globo>
2022-12-25 20:22:28 -0300

    persisting user in prisma

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -5,6 +5,7 @@ import { ArrowRight } from 'phosphor-react'
 import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
+import { api } from '../../lib/axios'
 import { Container, Form, FormError, Header } from './styles'
 
 const registerFormSchema = z.object({
@@ -43,7 +44,14 @@ export default function Register() {
   }, [setValue, router.query?.username])
 
   async function handleRegister({ name, username }: RegisterFormData) {
-    console.log(name, username)
+    try {
+      await api.post('/users', {
+        name,
+        username,
+      })
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
