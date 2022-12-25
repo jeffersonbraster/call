8a64aa9ec098b9c686b02359d24fc9cb6dd80080
Jefferson <jeferson.oliveira@g.globo>
2022-12-25 16:32:01 -0300

    initial config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: [
+    'page.tsx',
+    'page.ts',
+    'page.jsx',
+    'page.js',
+    'api.ts',
+    'api.tsx',
+  ],
 }
 
 module.exports = nextConfig
