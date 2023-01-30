04060f807e36fc37372e92ac86aca5cc22763536
Jefferson <jeferson.oliveira@g.globo>
2023-01-30 20:45:47 -0300

    persisting intervals

--- src/pages/api/users/time-intervals.api.ts
+++ src/pages/api/users/time-intervals.api.ts
@@ -1,7 +1,25 @@
 /* eslint-disable camelcase */
 import { NextApiRequest, NextApiResponse } from 'next'
 import { unstable_getServerSession } from 'next-auth'
+import { z } from 'zod'
+import { prisma } from '../../../lib/prisma'
 import { buildnextAuthOptions } from '../auth/[...nextauth].api'
+
+const timeIntervalsBodySchema = z.object({
+  intervals: z.array(
+    z.object({
+      weekDay: z.number().min(0).max(6),
+      startTimeInMinutes: z
+        .number()
+        .min(0)
+        .max(24 * 60),
+      endTimeInMinutes: z
+        .number()
+        .min(0)
+        .max(24 * 60),
+    }),
+  ),
+})
 
 export default async function handler(
   req: NextApiRequest,
@@ -17,5 +35,26 @@ export default async function handler(
     buildnextAuthOptions(req, res),
   )
 
-  return res.json({ session })
+  if (!session) {
+    return res.status(401).json({ error: 'Unauthorized' })
+  }
+
+  const { intervals } = timeIntervalsBodySchema.parse(req.body)
+
+  await Promise.all(
+    intervals.map((interval) => {
+      return prisma.userTimeInterval.create({
+        data: {
+          week_day: interval.weekDay,
+          time_start_in_minutes: interval.startTimeInMinutes,
+          time_end_in_minutes: interval.endTimeInMinutes,
+          user_id: session.user?.id,
+        },
+      })
+    }),
+  )
+
+  // await prisma.userTimeInterval.create
+
+  return res.status(201).end()
 }
