9411623b5af3af7401f4ba9715c8b05051e66699
Jefferson <jeferson.oliveira@g.globo>
2023-01-31 22:32:38 -0300

    create page schedule

---
+++ src/pages/schedule/[username]/index.page.tsx
@@ -0,0 +1,58 @@
+import { Avatar, Heading, Text } from '@ignite-ui/react'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { prisma } from '../../../lib/prisma'
+import { Container, UserHeader } from './styles'
+
+interface ScheduleProps {
+  user: {
+    name: string
+    bio: string
+    avatarUrl: string
+  }
+}
+
+export default function Schedule({ user }: ScheduleProps) {
+  return (
+    <Container>
+      <UserHeader>
+        <Avatar src={user.avatarUrl} />
+        <Heading>{user.name}</Heading>
+        <Text>{user.bio}</Text>
+      </UserHeader>
+    </Container>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const username = context.params?.username
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username: username as string,
+    },
+  })
+
+  if (!user) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    revalidate: 60 * 60 * 24, // 24 hours
+    props: {
+      user: {
+        name: user.name,
+        bio: user.bio,
+        avatarUrl: user.avatar_url,
+      },
+    },
+  }
+}
