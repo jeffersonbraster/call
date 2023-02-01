09b2565292740b189f9ef0e357ab989e4254ea69
Jefferson <jeferson.oliveira@g.globo>
2023-02-01 10:48:59 -0300

    create and styling list hours

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,10 +1,42 @@
 import Calendar from '../../../../../components/Calendar'
-import { Container } from './styles'
+import {
+  Container,
+  TimeHeader,
+  TimePicker,
+  TimePickerItem,
+  TimePickerList,
+} from './styles'
 
 export function CalendarStep() {
+  const hasSelectedDate = true
+
   return (
-    <Container>
+    <Container isTimePickerOpen={hasSelectedDate}>
       <Calendar />
+
+      {hasSelectedDate && (
+        <TimePicker>
+          <TimeHeader>
+            Terça-feira <span>20 de Janeiro</span>
+          </TimeHeader>
+
+          <TimePickerList>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>09:00h</TimePickerItem>
+            <TimePickerItem>10:00h</TimePickerItem>
+            <TimePickerItem>11:00h</TimePickerItem>
+            <TimePickerItem>12:00h</TimePickerItem>
+            <TimePickerItem>13:00h</TimePickerItem>
+            <TimePickerItem>14:00h</TimePickerItem>
+            <TimePickerItem>15:00h</TimePickerItem>
+            <TimePickerItem>16:00h</TimePickerItem>
+            <TimePickerItem>17:00h</TimePickerItem>
+            <TimePickerItem>18:00h</TimePickerItem>
+            <TimePickerItem>19:00h</TimePickerItem>
+            <TimePickerItem>20:00h</TimePickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
     </Container>
   )
 }
