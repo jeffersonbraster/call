489877ae047cedfe4ac8efbb27f4d69f2d16d687
Jefferson <jeferson.oliveira@g.globo>
2023-02-01 11:12:24 -0300

    create page form confirm

--- src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -1,3 +1,45 @@
+import { Button, Text, TextArea, TextInput } from '@ignite-ui/react'
+import { CalendarBlank, Clock } from 'phosphor-react'
+import { ConfirmForm, FormActions, FormHeader } from './styles'
+
 export function ConfirmStep() {
-  return <h1>confirm</h1>
+  function handleConfirmSchedule() {}
+
+  return (
+    <ConfirmForm as="form" onSubmit={handleConfirmSchedule}>
+      <FormHeader>
+        <Text>
+          <CalendarBlank />
+          22 de Fevereiro de 2022
+        </Text>
+
+        <Text>
+          <Clock />
+          18:00
+        </Text>
+      </FormHeader>
+
+      <label>
+        <Text size="sm">Nome completo</Text>
+        <TextInput placeholder="Seu nome" />
+      </label>
+
+      <label>
+        <Text size="sm">E-mail</Text>
+        <TextInput type="email" placeholder="Seu E-mail" />
+      </label>
+
+      <label>
+        <Text size="sm">Observações</Text>
+        <TextArea />
+      </label>
+
+      <FormActions>
+        <Button type="button" variant="tertiary">
+          Cancelar
+        </Button>
+        <Button type="submit">Confirmar</Button>
+      </FormActions>
+    </ConfirmForm>
+  )
 }
