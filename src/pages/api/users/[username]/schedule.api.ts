dde9740fe70c803a6bba05b3c86ea22ab9cbaf89
Jefferson <jeferson.oliveira@g.globo>
2023-02-03 20:18:10 -0300

    comunicando com api de calendar

--- src/pages/api/users/[username]/schedule.api.ts
+++ src/pages/api/users/[username]/schedule.api.ts
@@ -1,7 +1,9 @@
 /* eslint-disable camelcase */
 import dayjs from 'dayjs'
+import { google } from 'googleapis'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { z } from 'zod'
+import { getGoogleOAuthToken } from '../../../../lib/google'
 import { prisma } from '../../../../lib/prisma'
 
 export default async function handler(
@@ -50,13 +52,49 @@ export default async function handler(
     return res.status(400).json({ message: 'Scheduling already exists' })
   }
 
-  await prisma.scheduling.create({
+  const scheduling = await prisma.scheduling.create({
     data: {
       name,
       email,
       observations,
       date: schedulingDate.toDate(),
       user_id: user.id,
+    },
+  })
+
+  const calendar = google.calendar({
+    version: 'v3',
+    auth: await getGoogleOAuthToken(user.id),
+  })
+
+  await calendar.events.insert({
+    calendarId: 'primary',
+    conferenceDataVersion: 1,
+    requestBody: {
+      summary: `Call Jeje com: ${name}`,
+      description: `Email: ${email}\nObservações: ${observations}`,
+      start: {
+        dateTime: schedulingDate.format(),
+        timeZone: 'America/Sao_Paulo',
+      },
+      end: {
+        dateTime: schedulingDate.add(1, 'hour').format(),
+        timeZone: 'America/Sao_Paulo',
+      },
+      attendees: [
+        {
+          email,
+          displayName: name,
+        },
+      ],
+      conferenceData: {
+        createRequest: {
+          requestId: scheduling.id,
+          conferenceSolutionKey: {
+            type: 'hangoutsMeet',
+          },
+        },
+      },
     },
   })
 
